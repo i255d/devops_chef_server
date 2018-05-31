4dee6cd3765ae298d060b3465b6873e879283225
Dan <dxi02@acuitysso.com>
2018-05-31 14:41:55 -0400

    another test

---
+++ scripts/Acitivate.ps1
@@ -0,0 +1,17 @@
+﻿
+
+$path = 'C:\vsts\ea\iac-armt-chef\templates\'
+#'C:\tf\daniverson\Azure\03ChefLinux\'
+
+$devDeployParams = @{
+    subscription = 'IST-Dev'
+    resourceGroupName = 'djitestrhelRG5'
+    resourceGroupLocation = 'northcentralus'
+    deploymentName = 'djiDeployment'
+    templateFilePath = $path + 'template.json'
+    parametersFilePath = $path + 'parameters.json'
+}
+
+C:\tf\daniverson\Azure\03ChefLinux\deploy.ps1 @devDeployParams
+
+#$subscription = 'IST-Dev'
